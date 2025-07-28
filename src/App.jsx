fb14b913166b20d707345b2b501550b0261748f1
Sheetal Chaurasia <csheetal344@gmail.com>
2025-07-29 01:22:29 +0530

    Tic-Tac-Toe Game Fourth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Tictactoe from '../components/Tictactoe'
+import Tictactoe from './components/Tictactoe'
 
 function App() {
   const [count, setCount] = useState(0)
